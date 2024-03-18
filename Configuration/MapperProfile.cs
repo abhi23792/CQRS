13203d9bd0996aa0587b26fdc16ae00aa1683933
Abhimanyu Tandon <abhi.23792@gmail.com>
2024-03-18 10:00:44 +0530

    Initial commit for CQRS skeleton.

---
+++ Configuration/MapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using CQRS_Skeleton.Domain;
+using CQRS_Skeleton.Models;
+
+namespace CQRS_Skeleton.Configuration
+{
+    public class MapperProfile: Profile
+    {
+        public MapperProfile() 
+        {
+            CreateMap<Product, ProductDTO>();
+        }
+    }
+}
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using CQRS_Skeleton.Application.Query;
+using CQRS_Skeleton.Models;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CQRS_Skeleton.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ProductController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ProductDTO> GetProductAsync([FromQuery] GetProductsQuery query)
+        {
+            return await _mediator.Send(query);
+        }
+    }
+}
